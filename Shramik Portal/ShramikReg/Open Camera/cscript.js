0ab3b2ce28774ee0693cdb0681b4578b757523e4
Varad Patil <varadpatil466@gmail.com>
2024-04-15 18:30:24 +0530

    Setup Camera Permission Module

--- Shramik Portal/ShramikReg/Open Camera/cscript.js
+++ Shramik Portal/ShramikReg/Open Camera/cscript.js
@@ -2,5 +2,5 @@
 
 function openCameraPopup() {
     // Open a popup window with the camera stream
-    cameraPopup = window.open('camera.html', 'Camera', 'width=600,height=400');
+    cameraPopup = window.open('./Open Camera/camera.html', 'Camera', 'width=600,height=400');
 }
